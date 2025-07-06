09d18ff3d5811f16bd6a310713875cd4d0d71f0b
Claudio Sanhueza <=>
2025-07-06 17:37:44 -0400

    vistas base de la plantilla

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,72 @@
 @extends('layouts.app')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+{{-- Customize layout sections --}}
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+@section('subtitle', 'Welcome')
+@section('content_header_title', 'Home')
+@section('content_header_subtitle', '')
+@section('plugins.Datatables', true)
+@section('plugins.DatatablesPlugins', true)
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+{{-- Extend the browser title --}}
+
+{{-- Content body: main page content --}}
+
+@section('content_body')
+    {{-- Themes --}}
+    <x-adminlte-card class="shadow" title="Dark Card" theme="primary" icon="fas fa-lg fa-moon" collapsible maximizable>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod adipisci non earum et delectus mollitia voluptates ullam inventore consectetur itaque repudiandae rem doloribus, architecto voluptas voluptatum, ipsum temporibus quam at!
+
+        {{-- Setup data for datatables --}}
+        @php
+            $heads = [
+                'ID',
+                'Name',
+                ['label' => 'Phone', 'width' => 40],
+                ['label' => 'Actions', 'no-export' => true, 'width' => 5],
+            ];
+
+            $config['language'] = ['url' => 'https://cdn.datatables.net/plug-ins/1.11.3/i18n/es-cl.json'];
+            $config['order'] = [[0, 'desc']];
+            $config['responsive'] = true;
+            $config['fixHeader'] = true;
+        @endphp
+        <hr>
+        {{-- Minimal example / fill data using the component slot --}}
+        <x-adminlte-datatable id="table1" :heads="$heads" :config="$config" bordered hoverable striped beatify
+        with-buttons>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </x-adminlte-datatable>
+
+    </x-adminlte-card>
+@stop
+
+{{-- Push extra CSS --}}
+
+@push('css')
+    {{-- Add here extra stylesheets --}}
+    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+@endpush
+
+{{-- Push extra scripts --}}
+
+@push('js')
+    {{-- <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script> --}}
+@endpush
