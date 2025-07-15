fa8ab5fef481e915ea072313cd81c0c53a5e41bb
Claudio Sanhueza <=>
2025-07-14 22:49:29 -0400

    custom login con tabla personalizada

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Usuario;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $usuario = Usuario::where('usuario', Auth::user()->usuario)->first();
+        return view('home', compact('usuario'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,56 +5,22 @@
 @section('subtitle', 'Welcome')
 @section('content_header_title', 'Home')
 @section('content_header_subtitle', '')
-@section('plugins.Datatables', true)
-@section('plugins.DatatablesPlugins', true)
 
 {{-- Extend the browser title --}}
 
 {{-- Content body: main page content --}}
 
 @section('content_body')
-    {{-- Themes --}}
-    <x-adminlte-card class="shadow" title="Dark Card" theme="primary" icon="fas fa-lg fa-moon" collapsible maximizable>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod adipisci non earum et delectus mollitia voluptates ullam inventore consectetur itaque repudiandae rem doloribus, architecto voluptas voluptatum, ipsum temporibus quam at!
-
-        {{-- Setup data for datatables --}}
-        @php
-            $heads = [
-                'ID',
-                'Name',
-                ['label' => 'Phone', 'width' => 40],
-                ['label' => 'Actions', 'no-export' => true, 'width' => 5],
-            ];
-
-            $config['language'] = ['url' => 'https://cdn.datatables.net/plug-ins/1.11.3/i18n/es-cl.json'];
-            $config['order'] = [[0, 'desc']];
-            $config['responsive'] = true;
-            $config['fixHeader'] = true;
-        @endphp
-        <hr>
-        {{-- Minimal example / fill data using the component slot --}}
-        <x-adminlte-datatable id="table1" :heads="$heads" :config="$config" bordered hoverable striped beatify
-        with-buttons>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-        </x-adminlte-datatable>
-
+    <x-adminlte-card class="shadow" theme="primary">
+        Bienvenido
+        <x-adminlte-profile-widget
+            name="{{ $usuario->nombre.' '.$usuario->primer_apellido.' '.$usuario->segundo_apellido }}"
+            desc="{{ $usuario->perfil->perfil }}"
+            theme="primary"
+            img="{{ Avatar::create($usuario->nombre.' '.$usuario->primer_apellido.' '.$usuario->segundo_apellido)
+            ->toBase64() }}"
+            >
+        </x-adminlte-profile-widget>
     </x-adminlte-card>
 @stop
 
