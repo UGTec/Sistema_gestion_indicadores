4dffcb5b6904fa7de34d68e3088862b3a041f747
Claudio Sanhueza <=>
2025-07-17 01:11:44 -0400

    rutas para departamentos y estados de usuarios

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\DepartamentoController;
+use App\Http\Controllers\EstadoUsuarioController;
 
 Route::get('/', function () {
     return view('auth.login');
@@ -15,4 +17,12 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/home', [HomeController::class, 'index'])->name('home');
 
     Route::resource('usuarios', UsuarioController::class);
+    Route::resource('departamentos', DepartamentoController::class);
+    Route::resource('estados', EstadoUsuarioController::class)
+        ->parameters(
+            [
+                'estados' => 'estado'
+            ]
+        );
+
 });
