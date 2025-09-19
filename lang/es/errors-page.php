db0cc89dd1637dd406da98fa2e9f929d1b4fd637
Claudio Sanhueza <=>
2025-09-19 14:54:03 -0300

    http error custom

---
+++ lang/es/errors-page.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'Page Not Found'                                             => 'Página no encontrada',
+    'Sorry, the page you are looking for could not be found.'    => 'Lo sentimos, la página que buscas no se pudo encontrar.',
+    'Unauthorized'                                               => 'No autorizado',
+    'Sorry, you are not authorized to access this page.'         => 'Lo sentimos, no estás autorizado para acceder a esta página.',
+    'Whoops, something went wrong on our servers.'               => 'Vaya, algo salió mal en nuestros servidores.',
+    'Service Unavailable'                                        => 'Servicio no disponible',
+    'Sorry, the service is unavailable. Please try again later.' => 'Lo sentimos, el servicio no está disponible. Por favor, inténtalo de nuevo más tarde.',
+    'Back to Home'                                               => 'Volver al inicio',
+    'Forbidden'                                                  => 'Prohibido',
+    'Sorry, you do not have permission to access this page.'     => 'Lo sentimos, no tienes permiso para acceder a esta página.'
+];
